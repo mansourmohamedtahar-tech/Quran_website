4461a5c2ae2ae6f3eb6f1e3aa3e73aac8e3b798f
mansourmohamedtahar-tech <mansourmohamedtahar@gmail.com>
2026-06-19 11:04:51 +0200

    deuziéme commit

---
+++ script4.js
@@ -0,0 +1,53 @@
+
+$(document).ready(function(){
+            $('#btn1').click(function(){
+                $('#d1').slideToggle(1000)//slideToggle=show and hide
+               
+            })
+        });
+        $(document).ready(function(){
+            $('#btn2').click(function(){
+                $('#d2').slideToggle(1000)//slideToggle=show and hide
+              
+            })
+        });
+        $(document).ready(function(){
+            $('#btn3').click(function(){
+                $('#d3').slideToggle(1000)//slideToggle=show and hide
+             
+            })
+        });
+        $(document).ready(function(){
+            $('#btn4').click(function(){
+                $('#d4').slideToggle(1000)//slideToggle=show and hide
+                
+            })
+        });
+
+        $(document).ready(function(){
+            $('#btn5').click(function(){
+                $('#d5').slideToggle(1000)//slideToggle=show and hide
+               
+            })
+        });
+
+         $(document).ready(function(){
+            $('#btn6').click(function(){
+                $('#d6').slideToggle(1000)//slideToggle=show and hide
+               
+            })
+        });
+
+
+
+
+        // $(document).ready(function(){
+ //  $(window).scroll(function(){
+    //   var scrollpos= $(window).scrollTop();
+     //  $('.bg').css({
+     //     'height': +(scrollpos) + 'px'
+     //  })
+//  })
+//  })//ce script a le role de quand on scroll la photo vienne
+
+
